b4423617a7178b6962472821ef599021589f2d94
Sebastian Babb <sbabb@mail.sfsu.edu>
2016-02-16 21:24:25 -0800

    Modified SimpleServer to return http response 200 'OK' and some html to display.  Added StartServer.rb.  Updated README.md instructions.

--- README.md
+++ README.md
@@ -2,13 +2,5 @@
 CSc667 HTTP Server Project
 
 How to run:
-    1) Clone the repository.
-    2) Run irb from the root directory of SimpleServer.
-        $ cd SimpleServer && irb
-    3) From irb, load SimpleServer.rb and create a new instance of it
-       on port 8080.
-        > require_relative 'SimpleServer.rb'
-        > SimpleServer.new(8080).start
-    4) Open another terminal and run the client.
-        $ ./SimpleClient.rb
-
+1) Run StartServer.rb
+2) Open a web browser and navigate to localhost:8080
--- SimpleServer.rb
+++ SimpleServer.rb
@@ -1,23 +1,35 @@
-#!/usr/bin/ruby
+ #!/usr/bin/ruby
 
 require 'socket'
 
 class SimpleServer
     def initialize(port)
-        @port = 8080
+        @port = port
     end
 
     def start
         server = TCPServer.open @port 
 
+        # The message that will be dispayed in the browser.
+        message_body = "<html><body><h3>Hello, World!</h3></body></html>"
+
+        # The response message.
+        http_response = "HTTP/1.1 200 OK
+        Date: #{Time.new.to_s}
+        Server: OUR HTTP SERVER
+        Last-Modified: Wed, 22 Jul 2009 19:15:56 GMT
+        Content-Length: #{message_body.length}
+        Content-Type: text/html
+        Connection: Closed\n\n"
+
+        # Acknowledge the server is running.
         puts "Server started.  Listening on port #{@port}."
 
+        # Wait for requests.
         loop do
             client = server.accept
-            client.puts "Communication started at #{Time.now}"
-            client.puts "I am going to sleep for 5.0s now..."
-            sleep 5.0
-            client.puts "Communication ended at #{Time.now}"
+            client.puts http_response
+            client.puts message_body 
             client.close
         end
     end
