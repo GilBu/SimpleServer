f8bd9746fd964ed7389a67b6310a9c53f9d30224
jkrubin <jkrubin42@gmail.com>
2016-02-25 11:12:43 -0800

    process_line added on config... and it works

--- ConfigFile.rb
+++ ConfigFile.rb
@@ -1,10 +1,24 @@
 #!/usr/bin/ruby
 
 class ConfigFile
-    def initialize(config_array)
+
+    def initialize(new_config)
         # Do something
+        @config_array = new_config
     end
 
     # Implement me.
+
+    def process_line()
+
+    	@config_hash = {}
+
+    	@config_array.each do |key_value_pair|
+    		key , value = key_value_pair.split(" ")
+    		@config_hash[key.to_sym] = value
+    	end
+
+    	@config_hash
+    end
 
 end
